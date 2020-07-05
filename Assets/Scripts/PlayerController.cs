013567518654cc4101ed04dfa199150a39922acf
PranavSinha <pranavsinha188@gmail.com>
2020-07-05 22:17:10 +0530

    Horizontal input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Animator PlayerAnimator;
     void Start()
     {
         
@@ -13,6 +14,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float speed = Input.GetAxisRaw("Horizontal");
+        PlayerAnimator.SetFloat("Speed", Mathf.Abs(speed));
+
+        Vector2 scale = transform.localScale;
+        if (speed < 0)
+        {
+            scale.x = -1 * Mathf.Abs(scale.x);
+        }
+        else if (speed > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+
+        transform.localScale = scale;
+
     }
+
+ 
 }
