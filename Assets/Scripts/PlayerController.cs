563fb46b84f293f37c357ff15847b204dbeb9bb7
PranavSinha <pranavsinha188@gmail.com>
2020-07-06 23:25:41 +0530

    Run animation with movement input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,10 @@ public class PlayerController : MonoBehaviour
     BoxCollider2D PlayerCollider;
     Vector2 Playerpos;
     public float PlayerColliderNormalPosition, PlayerColliderCrouchPosition;
+    public float NormalSizex, NormalSizey;
+    public float CrouchSizex, CrouchSizey;
+    public float Speed;
+   private Vector2 PlayerPosition;
     void Start()
     {
         PlayerCollider = GetComponent<BoxCollider2D>();
@@ -18,40 +22,56 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
 
-        PlayerControl();
+        PlayerHorizontalMovement();
+        PlayerCrouch();
+        PlayerJump();
+        
+
     }
 
-    void PlayerControl()
+    void PlayerHorizontalMovement()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        PlayerAnimator.SetFloat("Speed", Mathf.Abs(speed));
-
+        float Horizontalspeed = Input.GetAxisRaw("Horizontal");
+        PlayerAnimator.SetFloat("Speed", Mathf.Abs(Horizontalspeed));
+        PlayerPosition = transform.position;
+        PlayerPosition.x += Horizontalspeed * Speed * Time.deltaTime;
+        transform.position = PlayerPosition;
         Vector2 scale = transform.localScale;
-        if (speed < 0)
+        if (Horizontalspeed < 0)
         {
             scale.x = -1 * Mathf.Abs(scale.x);
         }
-        else if (speed > 0)
+        else if (Horizontalspeed > 0)
         {
             scale.x = Mathf.Abs(scale.x);
         }
 
         transform.localScale = scale;
+    }
+
+    void PlayerCrouch()
+    {
+       
 
         if (Input.GetKey(KeyCode.LeftControl))
         {
             PlayerAnimator.SetBool("isCrouch", true);
-            PlayerCollider.size = new Vector2(1f, 1.2f);
+            PlayerCollider.size = new Vector2(CrouchSizex, CrouchSizey);
             PlayerCollider.offset = new Vector2(Playerpos.x, Playerpos.y + PlayerColliderCrouchPosition);
         }
         else
         {
             PlayerAnimator.SetBool("isCrouch", false);
-            PlayerCollider.size = new Vector2(0.5f, 2f);
+            PlayerCollider.size = new Vector2(NormalSizex, NormalSizey);
             PlayerCollider.offset = new Vector2(Playerpos.x, Playerpos.y + PlayerColliderNormalPosition);
 
         }
 
+        
+    }
+
+    void PlayerJump()
+    {
         if (Input.GetKey(KeyCode.W))
         {
             PlayerAnimator.SetBool("Jump", true);
